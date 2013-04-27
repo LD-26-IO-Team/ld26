5e74e1e0f98329bb5ee5088a4eafb333006a1a07
Vladyslav Tomashpolskyi <wizzardich@gmail.com>
2013-04-27 13:52:32 +0300

    Implemented object representation

--- ld26/src/io/github/ldears/ld26/map/GameObject.java
+++ ld26/src/io/github/ldears/ld26/map/GameObject.java
@@ -1,5 +1,7 @@
 package io.github.ldears.ld26.map;
 
+import java.awt.Point;
+
 /**
  * @author dector
  */
@@ -8,4 +10,20 @@ public abstract class GameObject {
 	String name;
 	int tiledX;
 	int tiledY;
+
+	protected GameObject(int x, int y, String name) {
+		this.tiledX = x;
+		this.tiledY = y;
+		this.name = name;
+	}
+
+	public boolean contains(Point p) {
+		if (p.x == tiledX && p.y == tiledY)
+			return true;
+		return false;
+	}
+	
+	public Point coordinates() {
+		return new Point(tiledX, tiledY);
+	}
 }
